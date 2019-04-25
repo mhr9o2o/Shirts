144e2bf45b70c41829dd0541da15087b350f9aa2
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-25 19:37:07 +0430

    testing gson conversion

--- app/src/test/java/com/mhr/shirts/data/data_models/ShirtTest.kt
+++ app/src/test/java/com/mhr/shirts/data/data_models/ShirtTest.kt
@@ -1,5 +1,6 @@
 package com.mhr.shirts.data.data_models
 
+import com.google.gson.Gson
 import org.junit.Assert
 import org.junit.Test
 
@@ -7,6 +8,7 @@ import org.junit.Assert.*
 
 class ShirtTest {
 
+    //region Equality Test
     @Test
     fun equalsForOtherType() {
         assertEquals(false, equals("A String"))
@@ -27,5 +29,31 @@ class ShirtTest {
         val second = Shirt(0, "Pants", 2000, "brown", 1, "xxl", "another_url")
         assertEquals(true, first == second)
     }
+    //endregion
+
+    //region Gson Test
+    @Test
+    fun convertingCorrectJsonToModel()
+    {
+        val json = "{\n" +
+                "    \"id\": 0,\n" +
+                "    \"price\": 88,\n" +
+                "    \"picture\": \"https://unsplash.it/128/128\",\n" +
+                "    \"colour\": \"brown\",\n" +
+                "    \"size\": \"m\",\n" +
+                "    \"name\": \"Southview Clarke\",\n" +
+                "    \"quantity\": 2\n" +
+                "  }"
+        val gson = Gson()
+        val convertedModel: Shirt = gson.fromJson(json, Shirt::class.java)
+        assertEquals(convertedModel.id, 0)
+        assertEquals(convertedModel.price,88)
+        assertEquals(convertedModel.picture,"https://unsplash.it/128/128")
+        assertEquals(convertedModel.colour,"brown")
+        assertEquals(convertedModel.size,"m")
+        assertEquals(convertedModel.name, "Southview Clarke")
+        assertEquals(convertedModel.quantity,2)
+    }
+    //endregion
 
 }
