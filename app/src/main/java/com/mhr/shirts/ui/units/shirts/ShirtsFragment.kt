8313fdb290102570657d6737bfe1af5faccedcec
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-25 21:54:07 +0430

    creating shirts fragment

---
+++ app/src/main/java/com/mhr/shirts/ui/units/shirts/ShirtsFragment.kt
@@ -0,0 +1,33 @@
+package com.mhr.shirts.ui.units.shirts
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.mhr.shirts.R
+
+class ShirtsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ShirtsFragment()
+    }
+
+    private lateinit var viewModel: ShirtsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.shirts_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ShirtsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/mhr/shirts/ui/units/shirts/ShirtsViewModel.kt
@@ -0,0 +1,7 @@
+package com.mhr.shirts.ui.units.shirts
+
+import androidx.lifecycle.ViewModel;
+
+class ShirtsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
