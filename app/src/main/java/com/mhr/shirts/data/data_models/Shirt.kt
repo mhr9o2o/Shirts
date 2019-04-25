b59a6db25bddbe7d5bf1b43442c5746e4741a483
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-25 19:13:49 +0430

    creating base shirt data class

---
+++ app/src/main/java/com/mhr/shirts/data/data_models/Shirt.kt
@@ -0,0 +1,3 @@
+package com.mhr.shirts.data.data_models
+
+data class Shirt(val id: Int, val name: String, val price: Int, val colour: String, val quantity: Int, val size: String, val picture: String)
