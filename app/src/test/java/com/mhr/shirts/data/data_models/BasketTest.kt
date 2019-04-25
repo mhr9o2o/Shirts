89baf9cfe2a8038a5d7a9946a0c5c767b8821a10
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-25 20:07:55 +0430

    testing json conversions

---
+++ app/src/test/java/com/mhr/shirts/data/data_models/BasketTest.kt
@@ -0,0 +1,30 @@
+package com.mhr.shirts.data.data_models
+
+import com.google.gson.Gson
+import org.junit.Assert.*
+import org.junit.Test
+
+class BasketTest {
+
+    @Test
+    fun convertingEmptyBasketToJson()
+    {
+        val emptyBasket = Basket()
+        val gson = Gson()
+        val expectedJson = "{\"shirts\":[]}"
+        val convertedJson = gson.toJson(emptyBasket)
+        assertEquals(expectedJson, convertedJson)
+    }
+
+    @Test
+    fun convertingBasketToJson()
+    {
+        val shirt = Shirt(0, "name", 20, "white", 3, "l", "url")
+        val basket = Basket(mutableListOf(shirt))
+        val expectedJson = "{\"shirts\":[{\"id\":0,\"name\":\"name\",\"price\":20,\"colour\":\"white\",\"quantity\":3,\"size\":\"l\",\"picture\":\"url\"}]}"
+        val gson = Gson()
+        val convertedJson = gson.toJson(basket)
+        assertEquals(expectedJson, convertedJson)
+    }
+
+}
