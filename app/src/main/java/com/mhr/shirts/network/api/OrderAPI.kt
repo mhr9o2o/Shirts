be7e5454d39b691ebae36a3ae4d34d5d42dc7017
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-25 23:29:28 +0430

    adding create methods to APIs

--- app/src/main/java/com/mhr/shirts/network/api/OrderAPI.kt
+++ app/src/main/java/com/mhr/shirts/network/api/OrderAPI.kt
@@ -3,6 +3,8 @@ package com.mhr.shirts.network.api
 import com.mhr.shirts.network.models.request.OrderRequest
 import com.mhr.shirts.network.models.response.SuccessfulOrderResponse
 import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -10,5 +12,18 @@ interface OrderAPI {
 
     @POST("/jayson")
     fun orderBasket(@Body order: OrderRequest): Observable<SuccessfulOrderResponse>
+
+    companion object
+    {
+        fun create(baseUrl: String): OrderAPI
+        {
+            val retrofitRequest = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .baseUrl(baseUrl)
+                .build()
+
+            return retrofitRequest.create(OrderAPI::class.java)
+        }
+    }
 
 }
--- app/src/main/java/com/mhr/shirts/network/api/ShirtsAPI.kt
+++ app/src/main/java/com/mhr/shirts/network/api/ShirtsAPI.kt
@@ -2,11 +2,26 @@ package com.mhr.shirts.network.api
 
 import com.mhr.shirts.data.data_models.Shirt
 import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.http.GET
 
 interface ShirtsAPI {
 
     @GET
     fun fetchShirts(): Observable<List<Shirt>>
+
+    companion object
+    {
+        fun create(baseUrl: String): ShirtsAPI
+        {
+            val retrofitRequest = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .baseUrl(baseUrl)
+                .build()
+
+            return retrofitRequest.create(ShirtsAPI::class.java)
+        }
+    }
 
 }
