893a605a73950b3b3049c1d32bcbbae770983c23
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-26 12:30:09 +0430

    creating the database

---
+++ app/src/main/java/com/mhr/shirts/data/database/AppDataBase.kt
@@ -0,0 +1,49 @@
+package com.mhr.shirts.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.mhr.shirts.data.data_models.Basket
+import com.mhr.shirts.data.data_models.Shirt
+import com.mhr.shirts.data.database.dao.BasketDao
+import com.mhr.shirts.data.database.dao.ShirtDao
+
+@Database(entities = [Shirt::class, Basket::class], version = 1)
+@TypeConverters(RoomTypeConverters::class)
+abstract class AppDataBase : RoomDatabase() {
+
+    abstract fun shirtDao(): ShirtDao
+    abstract fun basketDao(): BasketDao
+
+    companion object
+    {
+        var instance: AppDataBase? = null
+        val DATABASE_NAME = "ShirtsDB"
+
+        fun getAppDataBase(context: Context): AppDataBase?
+        {
+            if (instance == null)
+            {
+                synchronized(AppDataBase::class)
+                {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDataBase::class.java,
+                        DATABASE_NAME).build()
+                }
+            }
+
+            return instance
+
+        }
+
+        fun killDataBase()
+        {
+            instance = null
+        }
+
+    }
+
+}
