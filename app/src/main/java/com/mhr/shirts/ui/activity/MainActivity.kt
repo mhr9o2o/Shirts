6f58679c8a33120a6521141aa92c7ed0ecc3f5ab
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-26 17:40:23 +0430

    adding callback listener to bottom sheet

--- app/src/main/java/com/mhr/shirts/ui/activity/MainActivity.kt
+++ app/src/main/java/com/mhr/shirts/ui/activity/MainActivity.kt
@@ -2,12 +2,35 @@ package com.mhr.shirts.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.mhr.shirts.R
 
 class MainActivity : AppCompatActivity() {
+    //region Fields
 
+    //endregion
+
+    //region Overridden Functions
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomSheetView: View = findViewById(R.id.activity_main_bottom_sheet_fragment)
+        val rootView: View = findViewById(R.id.activity_main_root_fragment)
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetView)
+
+        bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                rootView.alpha = 1f - slideOffset
+            }
+        })
     }
+    //endregion
 }
