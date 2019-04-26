daa0c5a3edfd3827248242d8a35b1b8c4940c521
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-26 17:49:27 +0430

    handling onBackPressed, initialising both views and data in main activity

--- app/src/main/java/com/mhr/shirts/ui/activity/MainActivity.kt
+++ app/src/main/java/com/mhr/shirts/ui/activity/MainActivity.kt
@@ -7,10 +7,14 @@ import android.view.View
 import android.widget.FrameLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.mhr.shirts.R
+import com.mhr.shirts.data.DataAccessLayer
 
 class MainActivity : AppCompatActivity() {
-    //region Fields
 
+    //region Fields
+    var dataAccessLayer: DataAccessLayer? = null
+    var bottomSheetState: Int = BottomSheetBehavior.STATE_COLLAPSED
+    var bottomSheetBehavior: BottomSheetBehavior<View>? = null
     //endregion
 
     //region Overridden Functions
@@ -18,19 +22,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initViews()
+        initData()
+        
+    }
+
+    override fun onStop() {
+        super.onStop()
+        dataAccessLayer?.dispose()
+    }
+
+    override fun onBackPressed() {
+        if (bottomSheetState == BottomSheetBehavior.STATE_COLLAPSED)
+        {
+            super.onBackPressed()
+        }
+        else
+        {
+            bottomSheetBehavior?.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+
+    }
+    //endregion
+
+    //region Functions
+    fun initViews()
+    {
         val bottomSheetView: View = findViewById(R.id.activity_main_bottom_sheet_fragment)
         val rootView: View = findViewById(R.id.activity_main_root_fragment)
-        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetView)
+        val behavior = BottomSheetBehavior.from(bottomSheetView)
 
-        bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+        behavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
-
+                bottomSheetState = newState
             }
 
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
                 rootView.alpha = 1f - slideOffset
             }
         })
+
+        bottomSheetBehavior = behavior
+    }
+
+    fun initData()
+    {
+        dataAccessLayer = DataAccessLayer(this)
     }
     //endregion
 }
