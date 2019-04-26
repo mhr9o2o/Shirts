91067b1fb7fac5eb540f8df07507887e791ee30b
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-26 21:05:58 +0430

    adding name field to shirt item

--- app/src/main/java/com/mhr/shirts/ui/units/shirts/ShirtsAdapter.kt
+++ app/src/main/java/com/mhr/shirts/ui/units/shirts/ShirtsAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mhr.shirts.R
@@ -30,10 +31,21 @@ class ShirtsAdapter(private val shirts: List<Shirt>): RecyclerView.Adapter<Shirt
     {
         fun bindView(position: Int)
         {
-            if (position != -1 && position < shirts.size && shirts[position].picture != null)
+            if (position != -1 && position < shirts.size)
             {
                 val imageView = itemView.findViewById<AppCompatImageView>(R.id.item_shirt_image_view)
-                Glide.with(imageView).load(shirts[position].picture).into(imageView)
+                if (shirts[position].picture != null)
+                {
+                    Glide.with(imageView).load(shirts[position].picture).into(imageView)
+                }
+                else
+                {
+                    imageView.setImageResource(R.color.colorAccent)
+                }
+
+                val textView = itemView.findViewById<AppCompatTextView>(R.id.item_shirt_name_text_view)
+                textView.text = shirts[position].name
+
             }
         }
     }
