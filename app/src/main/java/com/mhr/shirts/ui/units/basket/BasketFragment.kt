8a4a822a28adc556acbb27f1ed10fedaa4e717b7
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-27 19:23:04 +0430

    implementing basket fragment general schema

--- app/src/main/java/com/mhr/shirts/ui/units/basket/BasketFragment.kt
+++ app/src/main/java/com/mhr/shirts/ui/units/basket/BasketFragment.kt
@@ -8,15 +8,57 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.mhr.shirts.R
+import com.mhr.shirts.data.data_models.Shirt
+import io.reactivex.disposables.CompositeDisposable
 
 class BasketFragment : Fragment() {
 
+    //region Fields
     companion object {
         fun newInstance() = BasketFragment()
     }
 
     private lateinit var viewModel: BasketViewModel
+    private val disposables = CompositeDisposable()
+    private lateinit var shirts: MutableList<Shirt>
+    //region Views
+    //endregion
 
+    //endregion
+
+    //region Functions
+    private fun initViews()
+    {
+
+    }
+
+    private fun initListeners()
+    {
+
+    }
+
+    private fun initList()
+    {
+
+    }
+
+    private fun adjustDataOnList(shirts: List<Shirt>)
+    {
+
+    }
+
+    private fun bind()
+    {
+
+    }
+
+    private fun unBind()
+    {
+        disposables.clear()
+    }
+    //endregion
+
+    //region Overridden Functions
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,7 +69,18 @@ class BasketFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(BasketViewModel::class.java)
-        // TODO: Use the ViewModel
+        disposables.add(viewModel.unitIsReady())
     }
+
+    override fun onResume() {
+        super.onResume()
+        bind()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unBind()
+    }
+    //endregion
 
 }
--- app/src/main/java/com/mhr/shirts/ui/units/basket/BasketViewModel.kt
+++ app/src/main/java/com/mhr/shirts/ui/units/basket/BasketViewModel.kt
@@ -1,11 +1,22 @@
 package com.mhr.shirts.ui.units.basket
 
 import androidx.lifecycle.ViewModel;
+import io.reactivex.disposables.Disposable
 
 class BasketViewModel : ViewModel() {
 
     //region Fields
-    val basketModel = BasketModel()
+    private val basketModel = BasketModel()
     //region
+
+    //region Model-relation Functions
+    fun unitIsReady() : Disposable
+    {
+        return basketModel.fetchData()
+    }
+    //endregion
+
+    //region View-relation Functions
+    //endregion
 
 }
