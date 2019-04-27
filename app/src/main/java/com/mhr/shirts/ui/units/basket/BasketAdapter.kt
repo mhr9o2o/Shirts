fca931989ae090dc60d916576e53d38f3dad9fe5
Mohammed Hossein <rahnama.m.h90@gmail.com>
2019-04-27 19:51:04 +0430

    implementing basket view holder

--- app/src/main/java/com/mhr/shirts/ui/units/basket/BasketAdapter.kt
+++ app/src/main/java/com/mhr/shirts/ui/units/basket/BasketAdapter.kt
@@ -1,8 +1,15 @@
 package com.mhr.shirts.ui.units.basket
 
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mhr.shirts.R
 import com.mhr.shirts.data.data_models.Shirt
 
-class BasketAdapter {
+class BasketAdapter(val shirts: List<Shirt>, val interactionListener: BasketItemInteractionsListener) {
 
     //region Fields
     //endregion
@@ -11,6 +18,72 @@ class BasketAdapter {
     //endregion
 
     //region ViewHolder
+    inner class BasketViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        //region Views
+        private val imageView: AppCompatImageView = itemView.findViewById(R.id.item_basket_image_view)
+        private val nameTextView: AppCompatTextView = itemView.findViewById(R.id.item_basket_name_text_view)
+        private val addButton: AppCompatImageButton = itemView.findViewById(R.id.item_basket_add_image_button)
+        private val removeButton: AppCompatImageButton = itemView.findViewById(R.id.item_basket_remove_image_button)
+        private val deleteButton: AppCompatImageButton = itemView.findViewById(R.id.item_basket_delete_image_button)
+        //endregion
+
+        //region Initializer
+        init {
+
+            addButton.setOnClickListener {
+                if (validatePosition(adapterPosition))
+                {
+                    interactionListener.onAddButtonClickedFor(shirts[adapterPosition])
+                }
+            }
+
+            removeButton.setOnClickListener {
+                if (validatePosition(adapterPosition))
+                {
+                    interactionListener.onRemoveButtonClickedFor(shirts[adapterPosition])
+                }
+            }
+
+            deleteButton.setOnClickListener {
+                if (validatePosition(adapterPosition))
+                {
+                    interactionListener.onDeleteButtonClickedFor(shirts[adapterPosition])
+                }
+            }
+
+        }
+        //endregion
+
+        //region Functions
+        fun bindView(position: Int)
+        {
+            if (validatePosition(position))
+            {
+                val shirt = shirts[position]
+
+                imageView.setImageResource(R.color.colorAccent)
+                if (shirt.picture != null)
+                {
+                    Glide.with(imageView).load(shirt.picture).into(imageView)
+                }
+
+                nameTextView.text = "${shirt.name}\nCount: ${shirt.quantity}"
+
+            }
+            else
+            {
+                imageView.setImageResource(R.color.colorAccent)
+                nameTextView.text = ""
+            }
+        }
+
+        private fun validatePosition(position: Int) : Boolean
+        {
+            return position != -1 && position < shirts.size
+        }
+        //endregion
+    }
     //endregion
 
     //region Callbacks
